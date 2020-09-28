aa3d293b07b2443c887f70bfdb3d962b44a45f50
Praveena2908 <70273041+Praveena2908@users.noreply.github.com>
2020-09-28 12:48:10 +0530

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://35.224.79.111:8080/QAWebapp/";
-	public String URL1 = "http://35.224.79.1118080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://35.226.24.137:8080/QAWebapp/";
+	public String URL1 = "http://35.226.24.1378080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
